de9de26af40dcf1c309dd58dd72fca8d38dc096d
psmor <morozovps@gmail.com>
2025-04-11 00:43:37 +0300

    Commit 2 (modul_02)

--- modul_02_Steam/src/main/java/main.java
+++ modul_02_Steam/src/main/java/main.java
@@ -19,41 +19,38 @@ public class main {
 
 
     public static void main(String[] args) {
-        /////////////////////////////////////////////////////////////////////////////////////////
-        // 3-е наибольшее число
-        int[] ints1 = {5,2,10,9,4,3,10,1,13};                          // На вход int
+        // 01 ///////////////////////////////////////////////////////////////////////////////////////
+        // 3-е наибольшее число (На вход int)
+        int[] ints1 = {5,2,10,9,4,3,10,1,13};
         int max11 = Arrays.stream(ints1)
+                //.boxed()                                  // Упаковка потока в оболочку (в данном случае int в Integer)
+                //.sorted(Collections.reverseOrder())       // Обратная сортировка. Требует передачи массива объектов, а не примитивов
+                .sorted()                                   // Сортировка по возрастанию
+                .limit(ints1.length-2)              // 3-е наибольшее
                 .max()
                 .getAsInt();
-        System.out.println("Максимальное число (1-й способ): " + max11);
+        System.out.println("3-е наибольшее число (1-й способ): " + max11);
 
-        Integer[] ints2 = new Integer[]{5,2,10,9,4,3,10,1,13};        // На вход Integer
+        // 3-е наибольшее число (На вход Integer)
+        Integer[] ints2 = new Integer[]{5,2,10,9,4,3,10,1,13};
         int max12 = Arrays.stream(ints2)
-                .max(Integer::compare)
+                .sorted(Collections.reverseOrder())        // Сортировка по убыванию
+                .limit(3)                          // 3-е наибольшее
+                .min(Integer::compare)
                 .get();
-        System.out.println("Максимальное число (2-й способ): " + max12);
+        System.out.println("3-е наибольшее число (2-й способ): " + max12);
 
-        /////////////////////////////////////////////////////////////////////////////////////////
-        // 3-е наибольшее уникальное число
-        Integer max21 = Arrays.stream(ints1)                            //На вход int
+        // 02 ///////////////////////////////////////////////////////////////////////////////////////
+        // 3-е наибольшее уникальное число (На вход Integer)
+        int max22 = Arrays.stream(ints2)
                 .distinct()
-                .boxed()                                               // Упаковка потока в оболочку (в данном случае int в Integer)
-                .sorted(Collections.reverseOrder())                    // Обратная сортировка. Требует передачи массива объектов
-                .limit(3)
-                .min(Comparator.comparing(Integer::valueOf))
-                .get();
-        System.out.println("3-е наибольшее уникальное число (1-й способ): "+max21);
-
-        int max22 = Arrays.stream(ints2)                               // На вход Integer
-                .distinct()
-                // здесь .boxed() не нужен, поскольку изначально поток из Integer
                 .sorted(Collections.reverseOrder())
                 .limit(3)
                 .min(Comparator.comparing(Integer::valueOf))
                 .get();
-        System.out.println("3-е наибольшее уникальное число (2-й способ): "+max22);
+        System.out.println("3-е наибольшее уникальное число: "+max22);
 
-        /////////////////////////////////////////////////////////////////////////////////////////
+        // 03 ///////////////////////////////////////////////////////////////////////////////////////
         // Список имён 3-х самыз старших сотрудников, с должностью "Инженер", в порядке убывания возраста
         List<String> names = employees.stream()
                 .filter(employee -> employee.getPost() == Post.ENGINEER)           // Оставляем только тех у кого должность ENGINEER
@@ -63,15 +60,7 @@ public class main {
                 .toList();
         System.out.println("3 самых старших сотрудника: "+names);
 
-        // Тестировал вывод в потоке
-//        employees.stream()
-//                .filter(employee -> employee.getPost() == Post.ENGINEER)           // Оставляем только тех у кого должность ENGINEER
-//                //.sorted(Comparator.comparingInt( (o-> o.getAge())) )               // Сортировка по возрасту, в порядке возрастания
-//                .sorted( Comparator.comparingInt(Employee :: getAge).reversed() )  // Сортировка по возрасту, в порядке убывания возраста
-//                .map(employee->employee.getName())                                 // Вывод только Имён
-//                .forEach(System.out::println);
-
-        /////////////////////////////////////////////////////////////////////////////////////////
+        // 04 ///////////////////////////////////////////////////////////////////////////////////////
         // Средний возраст сотрудников, с должностью "Инженер"
         Double averAge = employees.stream()
                 .filter(employee -> employee.getPost() == Post.ENGINEER)           // Оставляем только тех у кого должность ENGINEER
@@ -80,52 +69,47 @@ public class main {
                 .orElseThrow( ()-> new RuntimeException("Считать нечего") );       // На случай если поток будет пустым
         System.out.println("Средний возраст Инженеров = "+averAge);
 
-        /////////////////////////////////////////////////////////////////////////////////////////
+        // 05 ///////////////////////////////////////////////////////////////////////////////////////
         // Самое длинное слово
-        String[] strings = {"Длинное_слово", "Эх", "Упс"};
-        String str1 = Arrays.stream(strings)
-                .max(Comparator.comparingInt(String::length))
-                .get();
-        System.out.println("Самое длинное слово: "+str1);
+        String[] strings = {"Нет", "Длинное_слово", "Эх", "Упс"};
+        String str12 = Arrays.stream(strings)
+                //.max(Comparator.comparingInt(String::length))
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .findFirst()
+                .orElseThrow( () -> new RuntimeException("Пусто"));
+        System.out.println("Самое длинное слово: "+str12);
 
         /////////////////////////////////////////////////////////////////////////////////////////
         // Построить хеш мапы
-        String str2 = "шляпа солнце сакура море пляж шляпа";
-        //Arrays.stream(str2.split(" ")).forEach(System.out::println); // Вывожу массив строк
-
-        MainCollectors mainCollectors = new MainCollectors();
-        Map<String, Long> map = mainCollectors.getCountWord( Arrays.stream(str2.split(" ")).toList() );
+        String str2 = "шляпа солнце сакура море пляж шляпа шавка";
+        Map<String, Long> map2 =  Arrays.asList(str2.split(" ")).stream()
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
         System.out.println("Получилась мапа:");
-        for (String word : map.keySet()){
-            System.out.println("word: " + word + ", value: " + map.get(word));
+        for (String word : map2.keySet()){
+            System.out.println("word: " + word + ", value: " + map2.get(word));
         }
 
         /////////////////////////////////////////////////////////////////////////////////////////
         // Вывести слова в порядке увеличения длинны слова, если длинна одинаковая, тогда в алфовитном порядке.
         System.out.println("Строки в порядке увеличения длинны слова:");
         Arrays.stream(str2.split(" "))
-                .sorted()                                                     // Сначала сортируем по алфавиту
-                .sorted(Comparator.comparingInt(String :: length).reversed()) // Затем по длинне слова
+                // Сначала сортировка по длинне, обратная. Затем сортировка по алфавиту
+                .sorted( Comparator.comparing(String::length).reversed().thenComparing(String :: toString) )
                 .forEach(System.out::println);
 
         /////////////////////////////////////////////////////////////////////////////////////////
         //Массив строк, в каждом набор из 5 слов, разделённых пробелом. Нужно найти самое длинное.
         System.out.print("Самое длинное слово: ");
         String[] arr = {"шляпа солнце сакура море бриллиант",
-                        "погода навека длинный выгода последний"
+                "погода навека длинный выгода последний"
         };
+
         String str = Arrays.stream(arr)
-                       .map(x->x.split(" "))   // каждую строку представим в виде массива. Теперь имеем вложенны массив
-                       .flatMap(Stream::of)          // Разворачиваем вложеннвй массив в общий поток
-                       .max(Comparator.comparingInt(String::length)) // Самое длинное
-                       .get();
-                //.forEach(System.out::println);
-        System.out.println(str);
-
-
-
-
-
+                .map(x->x.split(" "))   // каждую строку представим в виде массива. Теперь имеем вложенны массив
+                .flatMap(Stream::of)          // Разворачиваем вложеннвй массив в общий поток
+                .sorted(Comparator.comparingInt(String::length).reversed()) // Сортировка обратная
+                .findFirst()
+                .orElseThrow( () -> new RuntimeException("Пусто"));
 
 
     }
