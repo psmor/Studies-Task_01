4d8c713fe83f5c54701de66766f2513dc63de1f9
psmor <morozovps@gmail.com>
2025-04-17 14:40:56 +0300

    Commit 2 (modul_02)

--- modul_03_Thread/src/main/java/Task/ThreadPoolExecutorStart.java
+++ modul_03_Thread/src/main/java/Task/ThreadPoolExecutorStart.java
@@ -4,23 +4,13 @@ public class ThreadPoolExecutorStart {
     public static void main(String[] args) {
         MainThredPolExecutor mainThredPolExecutor = new MainThredPolExecutor(4);
 
-//        for (int i = 0; i < 4; i++) {                   //  Накидываем задачи
-//            int j = i;
-//            mainThredPolExecutor.execute(                // Выполнение задачи
-//                    () -> UtilsMultiTreading.printMark(j)
-//            );
-//        }
-
-        mainThredPolExecutor.execute(() -> UtilsMultiTreading.printMark(1));
-        mainThredPolExecutor.execute(() -> UtilsMultiTreading.printMark(2));
-        mainThredPolExecutor.execute(() -> UtilsMultiTreading.printMark(3));
-        mainThredPolExecutor.execute(() -> UtilsMultiTreading.printMark(4));
-        mainThredPolExecutor.shutdown();                                       // Для теста остановки потока
-        //UtilsMultiTreading.sleepSec(1);
-        mainThredPolExecutor.execute(() -> UtilsMultiTreading.printMark(5));
-        mainThredPolExecutor.execute(() -> UtilsMultiTreading.printMark(6));
-        mainThredPolExecutor.execute(() -> UtilsMultiTreading.printMark(7));
-        mainThredPolExecutor.execute(() -> UtilsMultiTreading.printMark(8));
-
+        for (int i = 0; i < 10; i++) {                   //  Накидываем задачи
+            int j = i;
+            if (i >= 4) {mainThredPolExecutor.shutdown(); } // Для теста остановки потока
+            mainThredPolExecutor.execute(                // Выполнение задачи
+                    () -> UtilsMultiTreading.printMark(j)
+            );
+        }
+        mainThredPolExecutor.shutdown();
     }
 }
